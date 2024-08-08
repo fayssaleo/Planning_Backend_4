cef44217b065f55cc9e2bac9305e420f256bf1cb
Fayssal OUREZZOUQ <fayssal.ourezzouq@TangerAlliance.com>
2024-08-08 21:47:12 +0100

    first commit

---
+++ app/Modules/Box/routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Modules\Box\Http\Controllers\BoxController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group([
+    'prefix' => 'api/boxes',
+    'middleware' => ['auth:sanctum'],
+], function ($router) {
+    Route::post('/add', [BoxController::class,'add']);
+    Route::post('/updatePlanningAndBoxes', [BoxController::class,'updatePlanningAndBoxes']);
+    Route::post('/addPlanningAndBoxes', [BoxController::class,'addPlanningAndBoxes']);
+    Route::post('/getPlanningByIdAndBoxes', [BoxController::class,'getPlanningByIdAndBoxes']);
+    Route::post('/addPlanningBoxes', [BoxController::class,'addPlanningBoxes']);
+    Route::post('/getByPlanning', [BoxController::class,'getBoxesByPlanningId']);
+    Route::post('/update', [BoxController::class,'update']);
+});
